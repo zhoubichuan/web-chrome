fc2da6b184656b6013cff3b5774c376f1a68c785
周必川 <zhoubichuan@icloud.com>
2024-05-17 00:03:08 +0800

    更新文档

--- src/.vuepress/nav.js
+++ src/.vuepress/nav.js
@@ -2,75 +2,75 @@ const nav = [{
     text: '基础知识',
     items: [
         {
-            text: '一.DOM',
-            link: '/base/engine/1.index'
-        },
-        {
-            text: '二.浏览器 API',
-            link: '/base/project/1.index',
-        },
-        {
-            text: '三.网络',
-            link: '/base/vue2.x/1.index'
-        },
-        {
-            text: '四.权限',
-            link: '/base/vue3.x/1.index'
-        },
-        {
-            text: '四.浏览器对象',
+            text: '一.BOM对象',
             link: '/base/4.window/1.index'
         },
         {
-            text: '五.文档对象',
+            text: '二.DOM对象',
             link: '/base/5.document/1.index'
         },
         {
-            text: '三.浏览器',
-            link: '/base/3.browser/1.index'
+            text: '三.数据请求',
+            link: '/base/vue2.x/1.index'
         },
         {
-            text: '四.web网络',
-            link: '/base/4.network/1.index'
+            text: '四.权限验证',
+            link: '/base/vue3.x/1.index'
         },
+        {
+            text: '五.隐私安全',
+            link: '/base/engine/1.index'
+        },
+        {
+            text: '六.兼容性',
+            link: '/base/project/1.index',
+        },
+        // {
+        //     text: '三.浏览器',
+        //     link: '/base/3.browser/1.index'
+        // },
+        // {
+        //     text: '四.web网络',
+        //     link: '/base/4.network/1.index'
+        // },
     ]
 },
 {
     text: '高级知识',
     items: [
         {
-            text: '一.DOM Tree、CSSOM',
+            text: '一.v8引擎',
             link: '/senior/1.micro/1.index'
         },
 
-        {
-            text: '二.渲染、绘制',
-            link: '/senior/2.typescript/1.index'
-        },
-        {
-            text: '三.会话',
-            link: '/senior/3.deploy/1.index'
-        },
-        {
-            text: '四.事件循环',
-            link: '/senior/4.component/1.index'
-        },
-        {
-            text: '五.垃圾回收',
-            link: '/senior/5.garbage/1.index'
-        },
-        {
-            text: '六.webkit深入',
-            link: '/senior/6.webkit/1.index'
-        },
-        {
-            text: '七.引擎',
-            link: '/senior/7.engine/1.index'
-        },
-        {
-            text: '八.v8',
-            link: '/senior/8.v8/1.index'
-        },
+        // {
+        //     text: '二.渲染、绘制',
+        //     link: '/senior/2.typescript/1.index'
+        // },
+        // {
+        //     text: '三.会话',
+        //     link: '/senior/3.deploy/1.index'
+        // },
+        // {
+        //     text: '四.事件循环',
+        //     link: '/senior/4.component/1.index'
+        // },
+        // {
+        //     text: '五.垃圾回收',
+        //     link: '/senior/5.garbage/1.index'
+        // },
+        // {
+        //     text: '六.webkit深入',
+        //     link: '/senior/6.webkit/1.index'
+        // },
+        // {
+        //     text: '七.引擎',
+        //     link: '/senior/7.engine/1.index'
+        // },
+        // {
+        //     text: '八.v8',
+        //     link: '/senior/8.v8/1.index'
+        // },
     ]
 },
 {
